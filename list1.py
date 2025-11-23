b0a3d7d345250697e61b49370c22be420e0fb74e
batoulbel236 <batoulbel19@gmail.com>
2025-11-23 18:54:50 +0100

    now click  on commit

---
+++ list1.py
@@ -0,0 +1,27 @@
+#create an empty list
+list1 = []
+list2 = list()
+#insert an element in the list
+#insert a value in a given position 
+list1.insert(0,45)
+print(list1)
+
+list1.insert(0,25)
+print(list[1])
+
+list1.insert(6500,78)
+print(list1)
+
+list1.insert(1,78)
+print(list1)
+
+list1.append(98)
+print(list1)
+#how to insert at the last position using insert without using append
+my_list = [25,45,78]
+my_list.insert(len(my_list),98)
+print(my_list)
+
+
+
+
